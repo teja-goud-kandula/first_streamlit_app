9fca1a31cc23e3f92f2c96d43dc25a880dd4628a
Teja Goud Kandula <kandulateja6@gmail.com>
2022-12-26 07:21:02 +0530

    Update streamlit_app.py
    Updated the header message

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,4 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
-streamlit.header('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
+streamlit.header('Build Your Own Fruit Smoothie')
