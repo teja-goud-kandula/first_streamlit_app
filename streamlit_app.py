3e7837906e107f45567eb84146c143764af619c1
Teja Goud Kandula <kandulateja6@gmail.com>
2022-12-26 07:20:03 +0530

    Update streamlit_app.py
    Added a nee menu item

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
