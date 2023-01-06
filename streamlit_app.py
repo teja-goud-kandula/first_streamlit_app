401493cd72d172c7653cae903c00e2fc53ca9486
Teja Goud Kandula <kandulateja6@gmail.com>
2023-01-06 10:17:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,9 +44,6 @@ try:
 except URLError as e:
   streamlit.error()
 
-# don't run anything past here while we troubleshoot
-# streamlit.stop()
-
 streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
@@ -65,5 +62,6 @@ if streamlit.button('Get Fruit load list'):
 add_my_fruit = streamlit.text_input('What fruit would you like information about?', 'jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
 
-# Placeholder
+# don't run anything past here while we troubleshoot
+streamlit.stop()
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
